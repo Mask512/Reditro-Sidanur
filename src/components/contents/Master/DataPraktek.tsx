aa0b72c7c9155d06fa0d6e243a5a56b9efb5e878
Mask512 <mask.roktz@gmail.com>
2024-07-08 17:52:47 +0700

    user management data praktek

--- src/components/contents/Master/DataPraktek.tsx
+++ src/components/contents/Master/DataPraktek.tsx
@@ -1,10 +1,46 @@
-import { BreadCrumb } from "@/components/BreadCrumb"
+import { BreadCrumb } from '@/components/BreadCrumb';
+import { DataTable } from '@/components/ui/data-table';
+import { APP } from '@/data/app';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { dataPraktekColumns } from './dataPraktekColumns';
+
+export type DataPraktekType = {
+  id: string;
+  nama: string;
+  alamat: string;
+  bidan: string | null;
+};
+
+const endpoint = 'lokasi-prakteks';
 
 export const DataPraktek = () => {
+  const [dataLokasi, setDataLokasi] = useState<DataPraktekType[]>([]);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(`${APP.API_URL}/${endpoint}`);
+    if (data) {
+      setDataLokasi(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [setDataLokasi]);
+
+  const handleEdit = async (id:string, newData: DataPraktekType ) => {
+    await axios.put(`${APP.API_URL}/${endpoint}/${id}`, newData);
+    fetchData();
+  };
+
   return (
     <>
       <BreadCrumb pageName='Data Praktek'/>
       <h1 className="text-xl font-semibold">Data Praktek</h1>
+      <DataTable
+          columns={dataPraktekColumns(handleEdit)}
+          data={dataLokasi}
+        />
     </>
   )
 }
---
+++ src/components/contents/Master/dataPraktekColumns.tsx
@@ -0,0 +1,112 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { DataPraktekType } from './DataPraktek';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import React, { ChangeEvent, useState } from 'react';
+
+type DataPraktekFormProps = {
+  data: DataPraktekType;
+  handleEdit: (id: string, newData: DataPraktekType) => void;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+const DataPraktekForm: React.FC<DataPraktekFormProps> = ({
+  data,
+  handleEdit,
+}) => {
+  const [newData, setNewData] = useState<DataPraktekType>({
+    id: data.id,
+    nama: data.nama,
+    alamat: data.alamat,
+    bidan: data.bidan || null,
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSaveChanges = () => {
+    handleEdit(data.id, newData);
+  };
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Edit Data Lokasi Praktek</DialogTitle>
+        <DialogDescription>
+          Masukan data yang digunakan untuk mengubah data lokasi praktek.
+        </DialogDescription>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="nama" className="text-right">
+            Nama
+          </Label>
+          <Input
+            name="nama"
+            value={newData.nama}
+            onChange={handleChange}
+            className="col-span-3"
+          />
+        </div>
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="alamat" className="text-right">
+            Alamat
+          </Label>
+          <Input
+            name="alamat"
+            value={newData.alamat}
+            onChange={handleChange}
+            className="col-span-3"
+          />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button onClick={handleSaveChanges}>Simpan perubahan</Button>
+      </DialogFooter>
+    </>
+  );
+};
+
+export const dataPraktekColumns = (
+  handleEdit: (id: string, newData: DataPraktekType) => void,
+): ColumnDef<DataPraktekType>[] => [
+  {
+    accessorKey: 'nama',
+    header: 'Nama Lokasi Praktek',
+  },
+  {
+    accessorKey: 'alamat',
+    header: 'Alamat',
+  },
+  {
+    id: 'action',
+    header: 'Action',
+    cell: ({ row }) => {
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DataPraktekForm data={row.original} handleEdit={handleEdit} />
+          </DialogContent>
+        </Dialog>
+      );
+    },
+  },
+];

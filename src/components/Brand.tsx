51da2412a03ef0079cd8839e8485396178f29d97
Mask512 <mask.roktz@gmail.com>
2024-07-10 18:11:53 +0700

    fix brand height

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -10,11 +10,13 @@ interface BrandProps {
 export const Brand: React.FC<BrandProps> = ({ className }) => {
   return (
     <div
-      className={`flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 ${className}`}
+      className={`flex min-h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 ${className}`}
     >
       <Link to="/" className="flex items-center gap-2 font-semibold">
         <Hospital className="h-6 w-6" />
-        <span className="font-bold uppercase text-sm lg:text-xl">{APP.NAME}</span>
+        <span className="font-bold uppercase text-sm lg:text-xl">
+          {APP.NAME}
+        </span>
       </Link>
     </div>
   );
